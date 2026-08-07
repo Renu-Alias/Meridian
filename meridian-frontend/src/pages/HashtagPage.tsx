2acde67f039799b2d21ab73ce96a0e7b2073cb54
Renu Alias <renualiasmeleth@gmail.com>
2026-08-07 12:57:03 +0530

    Bug fix

--- meridian-frontend/src/pages/HashtagPage.tsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from 'react-router-dom';
-import { FeedPage } from './FeedPage';
-
-export function HashtagPage() {
-  const { tag } = useParams<{ tag: string }>();
-  return <FeedPage tag={tag} />;
-}
